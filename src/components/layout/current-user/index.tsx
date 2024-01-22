2546ec9633805870919d94cef7df85fa9682aa38
Alek Novkovski <alek@greatcode2.com>
2024-01-22 19:27:22 +0100

    Add enhanced user popover and create new Text component
    Popover content has been added inside the CurrentUser component, to show additional user information as well as a button which will toggle an upcoming account settings modal. In addition, a new Text component was created, incorporating a range of sizes and styles. This new component has been used to display the user's name in the popover content.

--- src/components/layout/current-user/index.tsx
+++ src/components/layout/current-user/index.tsx
@@ -3,17 +3,57 @@ import React from "react";
 import { useGetIdentity } from "@refinedev/core";
 import type { User } from "@/graphql/schema.types";
 
-import { Popover } from "antd";
+import { SettingOutlined } from "@ant-design/icons";
+import { Button, Popover } from "antd";
 import { CustomAvatar } from "../../custom-avatar";
+import { Text } from "../../text";
 
 
 export const CurrentUser = () => {
     const { data: user } = useGetIdentity<User>();
 
+    const content = (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <Text
+                strong
+                style={{
+                    padding: "12px 20px",
+                }}
+            >
+                {user?.name}
+            </Text>
+            <div
+                style={{
+                    borderTop: "1px solid #d9d9d9",
+                    padding: "4px",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "4px",
+                }}
+            >
+                <Button
+                    style={{ textAlign: "left" }}
+                    icon={<SettingOutlined />}
+                    type="text"
+                    block
+                    onClick={() => {null}}
+                >
+                    Account settings
+                </Button>
+            </div>
+        </div>
+    );
+
     return (
         <>
             <Popover
                 placement="bottomRight"
+                content={content}
                 trigger="click"
                 overlayInnerStyle={{ padding: 0 }}
                 overlayStyle={{ zIndex: 999 }}

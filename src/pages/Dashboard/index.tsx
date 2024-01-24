0f177075efa680537ed5f40d956e1a04ac6ec649
Alek Novkovski <alek@greatcode2.com>
2024-01-24 10:50:45 +0100

    Update Dashboard layout
    The existing Dashboard component layout in the application has been modified. Rows and columns were added from Ant Design to structure the layout and prepare for the integration of calendar and deals chart components.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import {Col, Row} from "antd";
 
 export function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <div>
+            <Row
+                gutter={[32, 32]}
+                style={{marginTop: '32px'}}
+            >
+                <Col
+                    xs={24}
+                    sm={24}
+                    xl={8}
+                    style={{height: '460px'}}
+                >
+                    <h1>CalendarUpcoming</h1>{/* TODO: Build this component   */}
+                </Col>
+                <Col
+                    xs={24}
+                    sm={24}
+                    xl={8}
+                    style={{height: '460px'}}
+                >
+                    <h1>DashboardDealsChart</h1>{/* TODO: Build this component   */}
+                </Col>
+            </Row>
+        </div>
     )
 }

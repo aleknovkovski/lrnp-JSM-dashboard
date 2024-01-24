0102e7e0de0688f85a454bf93428cd31c67bb161
Alek Novkovski <alek@greatcode2.com>
2024-01-24 13:00:27 +0100

    UpcomingEvents component: Build out card look
    This commit expands the skeleton structure of the UpcomingEvents component by importing and implementing the Card component from the Ant Design library. The card layout includes styles and a title setup with an icon and text. This change improves the visual display of the UpcomingEvents component.

--- src/components/dashboard/upcoming-events.tsx
+++ src/components/dashboard/upcoming-events.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
+import { Card } from 'antd'
+import {CalendarOutlined} from "@ant-design/icons";
+import {Text} from "@/components/text";
 export default function UpcomingEvents() {
     return (
-        <div>UpcomingEvents</div>
+        <Card
+            style={{
+                height: "100%",
+            }}
+            headStyle={{ padding: "8px 16px" }}
+            bodyStyle={{
+                padding: "0 1rem",
+            }}
+            title={
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "8px",
+                    }}
+                >
+                    <CalendarOutlined />
+                    <Text size="sm" style={{ marginLeft: ".7rem" }}>
+                        Upcoming events
+                    </Text>
+                </div>
+            }
+        > </Card>
     )
 }

3bfa2825042a21e85aa0fb9b030b4e1ead0aff4e
Alek Novkovski <alek@greatcode2.com>
2024-01-24 13:38:42 +0100

    UpcomingEvents: Add data loading state and data ui markup (w placeholders)
    Added a loading state and related UI to the UpcomingEvents component, with a conditional render based on the isLoading state. By monitoring this state variable, we can show a loading list first and switch to the actual content when it's ready. For now, it shows dummy data, but will show actual data (or loading skeletons) as we continue development.

--- src/components/dashboard/upcoming-events.tsx
+++ src/components/dashboard/upcoming-events.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-import { Card } from 'antd'
+import { Card, List } from "antd";
 import {CalendarOutlined} from "@ant-design/icons";
 import {Text} from "@/components/text";
 export default function UpcomingEvents() {
+    const [isLoading, setIsLoading] = React.useState(true);
+
     return (
         <Card
             style={{
@@ -27,6 +29,30 @@ export default function UpcomingEvents() {
                     </Text>
                 </div>
             }
-        > </Card>
+        >
+            {isLoading ? (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={["loading1", "loading2", "loading3", "loading4", "loading5"]}
+                    renderItem={(item) => (
+                        <List.Item>
+                            {item}
+                        </List.Item>
+                    )}
+                >
+                </List>
+            ) : (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={[1, 2, 3, 4, 5]}
+                    renderItem={(item) => (
+                        <List.Item>
+                            {item}
+                        </List.Item>
+                    )}
+                >
+                </List>
+            )}
+        </Card>
     )
 }

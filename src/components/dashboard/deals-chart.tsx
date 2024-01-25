47bf28af4eeab8bc62d7fd08d33647ca80f94705
Alek Novkovski <alek@greatcode2.com>
2024-01-25 15:42:04 +0100

    Add data fetching and mapping to DealsChart component
    The DealsChart component has been updated to fetch and map data for visual representation. The fetched data is based on specific "WON" and "LOST" deal stages. Additionally, the mapping of the data was done using a utility helper function for a more streamlined chart representation.

--- src/components/dashboard/deals-chart.tsx
+++ src/components/dashboard/deals-chart.tsx
@@ -3,8 +3,35 @@ import {DollarOutlined} from "@ant-design/icons";
 import { Card } from 'antd';
 
 import { Text } from "@/components/text";
+import {Area} from "@ant-design/plots";
+import {mapDealsData} from "@/utilities/helpers";
+import {DASHBOARD_DEALS_CHART_QUERY} from "@/graphql/queries";
+import {GetFieldsFromList} from "@refinedev/nestjs-query";
+import {DashboardDealsChartQuery} from "@/graphql/types";
+import {useList} from "@refinedev/core";
 
 export default function DealsChart() {
+
+    const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+        resource: "dealStages",
+        filters: [{ field: "title", operator: "in", value: ["WON", "LOST"] }],
+        meta: {
+            gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+        },
+    });
+
+
+    const dealData = React.useMemo(() => {
+        return mapDealsData(data?.data);
+    }, [data?.data]);
+
+    const config = {
+        data: dealData,
+        xField: "timeText",
+        yField: "value",
+        seriesField: "state",
+    }
+
     return (
         <Card
             style={{ height: "100%" }}
@@ -25,7 +52,7 @@ export default function DealsChart() {
                 </div>
             }
         >
-            Deals Chart
+            <Area {...config} height={325} />
         </Card>
     )
 }

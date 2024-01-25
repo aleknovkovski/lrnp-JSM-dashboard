6a7927198c2bc0ffc35776fd04d4b308dcbf7cb8
Alek Novkovski <alek@greatcode2.com>
2024-01-25 12:34:12 +0100

    Updated DealsChart with base styling and structure
    The DealsChart component has been updated with a basic layout implementation. This not only includes styling but also the base structure within a Card component. Additionally, a DollarOutlined icon and 'Deals Chart' text are added as the Card's title.

--- src/components/dashboard/deals-chart.tsx
+++ src/components/dashboard/deals-chart.tsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import {DollarOutlined} from "@ant-design/icons";
+import { Card } from 'antd';
+
+import { Text } from "@/components/text";
 
 export default function DealsChart() {
     return (
-        <div>DealsChart</div>
+        <Card
+            style={{ height: "100%" }}
+            headStyle={{ padding: "8px 16px" }}
+            bodyStyle={{ padding: "24px 24px 0px 24px" }}
+            title={
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "8px",
+                    }}
+                >
+                    <DollarOutlined />
+                    <Text size="sm" style={{ marginLeft: ".5rem" }}>
+                        Deals Chart
+                    </Text>
+                </div>
+            }
+        >
+            Deals Chart
+        </Card>
     )
 }
